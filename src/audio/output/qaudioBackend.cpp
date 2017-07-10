6b4aa5c2e919158aea64c74dd8179c9762d6519b
Dr. Fiemost <drfiemost@users.sourceforge.net>
2017-07-10 21:06:36 +0200

    Add experimental QtMultiMedia backend

---
+++ src/audio/output/qaudioBackend.cpp
@@ -0,0 +1,123 @@
+/*
+ *  Copyright (C) 2006-2017 Leandro Nini
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "qaudioBackend.h"
+
+#include <QAudioDeviceInfo>
+#include <QAudioFormat>
+#include <QList>
+#include <QString>
+#include <QDebug>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+const char qaudioBackend::name[] = "QAUDIO";
+
+/*****************************************************************/
+
+qaudioBackend::qaudioBackend() :
+    outputBackend(name),
+    _audioOutput(nullptr),
+    _audioBuffer(nullptr),
+    _buffer(nullptr)
+{
+    // Check devices
+    QList<QAudioDeviceInfo> list = QAudioDeviceInfo::availableDevices(QAudio::AudioOutput);
+    for (QList<QAudioDeviceInfo>::const_iterator deviceInfo = list.constBegin();
+            deviceInfo != list.constEnd(); ++deviceInfo)
+    {
+        addDevice((*deviceInfo).deviceName().toUtf8().constData());
+    }
+}
+
+size_t qaudioBackend::open(const unsigned int card, unsigned int &sampleRate,
+                           const unsigned int channels, const unsigned int prec)
+{
+    QAudioFormat format;
+    format.setFrequency(sampleRate);
+    format.setChannels(channels);
+    format.setSampleSize(prec * 8);
+    format.setCodec("audio/pcm");
+    format.setByteOrder(QAudioFormat::LittleEndian);
+    format.setSampleType(prec == 1 ? QAudioFormat::UnSignedInt : QAudioFormat::SignedInt);
+
+    QList<QAudioDeviceInfo> list = QAudioDeviceInfo::availableDevices(QAudio::AudioOutput);
+
+    _audioOutput = new QAudioOutput(list[card], format);
+    if (_audioOutput->error() != QAudio::NoError)
+    {
+        qDebug("error");
+    }
+
+    data.resize(0);
+    _audioBuffer = new QBuffer(&data);
+    _audioBuffer->open(QIODevice::ReadWrite);
+    _audioOutput->start(_audioBuffer);
+
+    if (_audioOutput->error() != QAudio::NoError)
+    {
+        delete _audioOutput;
+        _audioOutput = nullptr;
+        delete _audioBuffer;
+        return 0;
+    }
+qDebug() << "bufferSize: " << _audioOutput->bufferSize() << " bytes";
+    _buffer = new char[_audioOutput->bufferSize()];
+    return _audioOutput->bufferSize();
+}
+
+void qaudioBackend::close()
+{
+    delete _audioOutput;
+    _audioOutput = nullptr;
+
+    delete _audioBuffer;
+    delete [] _buffer;
+}
+
+bool qaudioBackend::write(void* buffer, size_t bufferSize)
+{
+    data.append((const char*)buffer, bufferSize);
+
+    // FIXME this sucks
+    while (!_audioBuffer->atEnd() && _audioOutput->state() != QAudio::StoppedState)
+        usleep(10);
+
+    return true;
+}
+
+void qaudioBackend::stop()
+{
+    _audioOutput->stop();
+}
+
+void qaudioBackend::volume(int vol)
+{
+    qDebug("Unimplemented");
+    //_audioOutput->volume(qreal(vol/100.0f));
+}
+
+int qaudioBackend::volume()
+{
+    qDebug("Unimplemented");
+    return 0;
+    // return _audioOutput->volume()*100;
+}
---
+++ src/audio/output/qaudioBackend.h
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (C) 2006-2017 Leandro Nini
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef QAUDIOBACKEND_H
+#define QAUDIOBACKEND_H
+
+#include <QAudioOutput>
+#include <QBuffer>
+#include <QByteArray>
+
+#include "outputBackend.h"
+
+/*****************************************************************/
+
+/**
+ * QAudio output backend
+ */
+class qaudioBackend : public outputBackend
+{
+private:
+    QAudioOutput *_audioOutput;
+
+    QBuffer *_audioBuffer;
+    QByteArray data;
+
+    char *_buffer;
+
+private:
+    qaudioBackend();
+
+public:
+    virtual ~qaudioBackend() {};
+
+    static const char name[];
+
+    /// Factory method
+    static output* factory() { return new qaudioBackend(); }
+
+    /// Open
+    size_t open(const unsigned int card, unsigned int &sampleRate, const unsigned int channels, const unsigned int prec);
+
+    /// Close
+    void close();
+
+    /// Get buffer
+    void *buffer() { return _buffer; }
+
+    /// Write data to output
+    bool write(void* buffer, size_t bufferSize);
+
+    /// Stop
+    void stop();
+
+    /// Set volume
+    void volume(int vol);
+
+    /// Get volume
+    int volume();
+};
+
+#endif
