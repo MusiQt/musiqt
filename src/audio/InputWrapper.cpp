MusiQt/musiqt

commit 6ca2cad053abd3819c103e4e86db23d01ae49f5a

Handle song times in milliseconds

src/audio/InputWrapper.cpp
@@ -42,7 +42,7 @@ InputWrapper::InputWrapper(input* song) :
     m_preloadedSong(nullptr),
     m_audioConverter(nullptr),
     m_bytes(0),
-    m_seconds(0)
+    m_milliSeconds(0)
 {}
 
 InputWrapper::~InputWrapper()
@@ -69,12 +69,12 @@ PROFILE_START
         size_t size = m_currentSong->fillBuffer(
             m_audioConverter->buffer(),
             m_audioConverter->bufSize(maxSize),
-            m_seconds);
+            m_milliSeconds);
         n = m_audioConverter->convert(data, size);
     }
     else
     {
-        n = m_currentSong->fillBuffer(data, maxSize, m_seconds);
+        n = m_currentSong->fillBuffer(data, maxSize, m_milliSeconds);
     }
 
     m_audioProcess->process(data, n);
@@ -99,7 +99,7 @@ qint64 InputWrapper::readData(char *data, qint64 maxSize)
         {
             m_currentSong = m_preloadedSong;
             m_preloadedSong = nullptr;
-            m_seconds = 0;
+            m_milliSeconds = 0;
             n = fillBuffer(data, maxSize);
             emit switchSong();
         }
@@ -111,13 +111,14 @@ qint64 InputWrapper::readData(char *data, qint64 maxSize)
     }
 
     m_bytes += n;
-    if (m_bytes > m_bytePerSec)
+    int oldSeconds = m_milliSeconds/1000;
+    do {
+        m_bytes -= m_bytePerMilliSec;
+        m_milliSeconds++;
+    } while (m_bytes > m_bytePerMilliSec);
+    if (oldSeconds != (m_milliSeconds/1000))
     {
-        do {
-            m_bytes -= m_bytePerSec;
-            m_seconds++;
-        } while (m_bytes > m_bytePerSec);
-        if (m_seconds != m_currentSong->time()-5)
+        if (m_milliSeconds != m_currentSong->time()-5)
             emit updateTime();
         else
             emit preloadSong();
@@ -157,7 +158,7 @@ void InputWrapper::unload()
 
 void InputWrapper::setPos(int pos)
 {
-    m_seconds = (pos * m_currentSong->time()) / 100;
+    m_milliSeconds = (pos * m_currentSong->time()) / 100;
 }
 
 void InputWrapper::setFormat(int sampleRate, int channels, sample_t sampleType, int bufferSize)
@@ -181,7 +182,7 @@ void InputWrapper::setFormat(int sampleRate, int channels, sample_t sampleType, 
         break;
     }
 
-    m_bytePerSec = sampleRate * channels * precision;
+    m_bytePerMilliSec = (sampleRate * channels * precision) / 1000;
 
     // Check if soundcard supports requested samplerate
     m_audioConverter = CFACTORY->get(m_currentSong->samplerate(), sampleRate, bufferSize,

src/audio/input/input.h
@@ -90,7 +90,7 @@ public:
     virtual bool seek(const int pos) =0;
 
     /// Callback function
-    virtual size_t fillBuffer(void* buffer, const size_t bufferSize, const unsigned int seconds) =0;
+    virtual size_t fillBuffer(void* buffer, const size_t bufferSize, const unsigned int milliSeconds) =0;
 
     /// Song is loaded
     virtual QString songLoaded() const =0;

src/audio/input/inputBackend.h
@@ -38,7 +38,7 @@ private:
     const char *_name;
     QIcon _icon;
 
-    unsigned int _time;
+    unsigned int m_time;
 
 protected:
     metaDataImpl _metaData;
@@ -54,7 +54,7 @@ protected:
     inputBackend(const char name[], const unsigned char iconType[]=nullptr, unsigned int iconLen=0);
 
     /// Set song duration
-    void time(unsigned int newTime) { _time=newTime; }
+    void time(unsigned int newTime) { m_time = newTime; }
 
     /// Load int setting
     int load(const char* key, int defVal)
@@ -92,8 +92,8 @@ public:
     /// Get song info
     virtual const metaData* getMetaData() const override { return &_metaData; }
 
-    /// Get song duration
-    unsigned int time() const override { return _time; }
+    /// Get song duration in seconds
+    unsigned int time() const override { return m_time / 1000; }
 
     /// Get fractional scale for fixed point types
     virtual unsigned int fract() const override { return 0; }

src/audio/InputWrapper.h
@@ -54,7 +54,7 @@ public:
 
     void setFormat(int sampleRate, int channels, sample_t sampleType, int bufferSize);
 
-    int getSeconds() const { return m_seconds; }
+    int getSeconds() const { return m_milliSeconds/1000; }
 
     void setPos(int pos);
 
@@ -67,8 +67,8 @@ private:
     converter *m_audioConverter;
 
     int m_bytes;
-    int m_bytePerSec;
-    unsigned int m_seconds;
+    int m_bytePerMilliSec;
+    unsigned int m_milliSeconds;
 };
 
 #endif

src/audio/input/ffmpegBackend.cpp
@@ -81,7 +81,7 @@ QStringList ffmpegBackend::_ext;
 
 /*****************************************************************/
 
-size_t ffmpegBackend::fillBuffer(void* buffer, const size_t bufferSize, const unsigned int seconds)
+size_t ffmpegBackend::fillBuffer(void* buffer, const size_t bufferSize, const unsigned int milliSeconds)
 {
     size_t n = decodeBufOffset;
     unsigned char* buf = decodeBuf;
@@ -340,7 +340,7 @@ bool ffmpegBackend::open(const QString& fileName)
     _metaData.addInfo(metaData::TRACK, getMetadata("track"));
     _metaData.addInfo(metaData::COMMENT, getMetadata("comment"));
 
-    time(fc->duration/1000000);
+    time(fc->duration/1000);
 
     songLoaded(fileName);
     return true;

src/audio/input/ffmpegBackend.h
@@ -153,7 +153,7 @@ public:
     sample_t precision() const override { return _precision; }
 
     /// Callback function
-    size_t fillBuffer(void* buffer, const size_t bufferSize, const unsigned int seconds) override;
+    size_t fillBuffer(void* buffer, const size_t bufferSize, const unsigned int milliSeconds) override;
 
     /// Open config dialog
     QWidget* config(QWidget* win) override { return new ffmpegConfig(win); }

src/audio/input/gmeBackend.cpp
@@ -52,7 +52,7 @@ QStringList gmeBackend::_ext;
 
 /*****************************************************************/
 
-size_t gmeBackend::fillBuffer(void* buffer, const size_t bufferSize, const unsigned int seconds)
+size_t gmeBackend::fillBuffer(void* buffer, const size_t bufferSize, const unsigned int milliSeconds)
 {
     if (gme_track_ended(_emu))
         return 0;
@@ -201,7 +201,7 @@ void gmeBackend::getInfo()
     if (comment.isEmpty())
         _metaData.addInfo(metaData::COMMENT, ti->comment);
 
-    time(ti->length/1000);
+    time(ti->length);
 
     gme_free_info(ti);
 }

src/audio/input/gmeBackend.h
@@ -127,7 +127,7 @@ public:
     sample_t precision() const override { return sample_t::S16; }
 
     /// Callback function
-    size_t fillBuffer(void* buffer, const size_t bufferSize, const unsigned int seconds) override;
+    size_t fillBuffer(void* buffer, const size_t bufferSize, const unsigned int milliSeconds) override;
 
     /// Open config dialog
     QWidget* config(QWidget* win) override { return new gmeConfig(win); }

commit d0db2c7e5c236a593fa60e8ae9bf1fe4f8947698

Fix seeking broken with 6ca2cad

src/audio/InputWrapper.cpp
@@ -118,7 +118,7 @@ qint64 InputWrapper::readData(char *data, qint64 maxSize)
     } while (m_bytes > m_bytePerMilliSec);
     if (oldSeconds != (m_milliSeconds/1000))
     {
-        if (m_milliSeconds != m_currentSong->time()-5)
+        if (m_milliSeconds != m_currentSong->songDuration()-5)
             emit updateTime();
         else
             emit preloadSong();
@@ -158,7 +158,7 @@ void InputWrapper::unload()
 
 void InputWrapper::setPos(int pos)
 {
-    m_milliSeconds = (pos * m_currentSong->time()) / 100;
+    m_milliSeconds = (pos * m_currentSong->songDuration()) / 100;
 }
 
 void InputWrapper::setFormat(int sampleRate, int channels, sample_t sampleType, int bufferSize)

src/audio/input/input.h
@@ -59,8 +59,8 @@ public:
     /// Get song info
     virtual const metaData* getMetaData() const =0;
 
-    /// Get song duration
-    virtual unsigned int time() const =0;
+    /// Get song duration in milliseconds
+    virtual unsigned int songDuration() const =0;
 
     /// Get samplerate
     virtual unsigned int samplerate() const =0;

src/audio/input/inputBackend.h
@@ -92,8 +92,8 @@ public:
     /// Get song info
     virtual const metaData* getMetaData() const override { return &m_metaData; }
 
-    /// Get song duration in seconds
-    unsigned int time() const override { return m_time / 1000; }
+    /// Get song duration in milliseconds
+    unsigned int songDuration() const override { return m_time; }
 
     /// Get fractional scale for fixed point types
     virtual unsigned int fract() const override { return 0; }

src/audio/input/inputFactory.cpp
@@ -62,7 +62,7 @@ class nullInput : public input
 {
 public:
     const metaData* getMetaData() const override { return nullptr; }
-    unsigned int time() const override { return 0; }
+    unsigned int songDuration() const override { return 0; }
     unsigned int samplerate() const override { return 0; }
     unsigned int channels() const override { return 0; }
     sample_t precision() const override { return sample_t::S16; }

src/gui/centralFrame.cpp
@@ -668,7 +668,7 @@ void centralFrame::onUpdateTime()
         emit updateTime(m_audio->seconds());
 
         if (!m_slider->isSliderDown())
-            emit updateSlider((100*m_audio->seconds())/m_input->time());
+            emit updateSlider((100*m_audio->seconds()*1000L)/m_input->songDuration());
     }
 }
 

src/gui/mainWindow.cpp
@@ -473,7 +473,7 @@ void mainWindow::setPlayButton(state_t state)
 void mainWindow::setDisplay(input* i)
 {
     m_timeDisplay->setTime(0);
-    m_songTime->setTime(i->time());
+    m_songTime->setTime(i->songDuration() / 1000);
 
     m_subtunes->setText(QString("%1/%2").arg(i->subtune()).arg(i->subtunes()));
 