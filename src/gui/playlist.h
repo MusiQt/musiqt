897755607108f85700f4a81d9cda510aa445a836
Leandro Nini <drfiemost@users.noreply.github.com>
2021-04-08 21:38:06 +0200

    Drop redundant signal

--- src/gui/playlist.h
+++ src/gui/playlist.h
@@ -21,25 +21,16 @@
 
 #include <QListView>
 #include <QMouseEvent>
+#include <QDebug>
 
 class playlist : public QListView
 {
     Q_OBJECT
 
-signals:
-    void changed();
-
 private:
     playlist() {}
     playlist(const playlist&);
     playlist& operator=(const playlist&);
-
-protected:
-    void paintEvent(QPaintEvent *event) override
-    {
-        emit changed();
-        QListView::paintEvent(event);
-    }
 
 public:
     playlist(QWidget * parent) :
