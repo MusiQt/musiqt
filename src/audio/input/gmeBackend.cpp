MusiQt/musiqt

commit 94cdbcd644cbe64371e5c312b28fa0e963fa6302

input: drop the close function

src/audio/input/gmeBackend.cpp
@@ -33,7 +33,7 @@
 
 #include <string>
 
-#define EXT "ay|gbs|gym|hes|kss|nsfe|nsf|sap|spc|vgm|vgz"
+#define EXT "ay|gbs|gym|hes|f|nsfe|nsf|sap|spc|vgm|vgz"
 
 // ASMA path to STIL.
 #define ASMA_STIL "/Docs/STIL.txt"
@@ -121,7 +121,8 @@ gmeBackend::gmeBackend() :
 
 gmeBackend::~gmeBackend()
 {
-    close();
+    gme_delete(_emu);
+// 
 #ifdef HAVE_STILVIEW
     delete _stil;
 #endif
@@ -129,8 +130,6 @@ gmeBackend::~gmeBackend()
 
 bool gmeBackend::open(const QString& fileName)
 {
-    close();
-
     gme_type_t fileType;
     if (!checkRetCode(gme_identify_file(fileName.toUtf8().constData(), &fileType)))
         return false;
@@ -227,14 +226,6 @@ bool gmeBackend::checkRetCode(const char* error)
         return false;
     }
     return true;
-}
-
-void gmeBackend::close()
-{
-    gme_delete(_emu);
-    _emu = nullptr;
-
-    songLoaded(QString());
 }
 
 bool gmeBackend::rewind()

src/audio/input/ffmpegBackend.cpp
@@ -241,11 +241,22 @@ ffmpegBackend::ffmpegBackend() :
     m_audioStream(nullptr),
     m_formatContext(nullptr),
     m_codecContext(nullptr),
-    m_config(name, iconFfmpeg, 86) {}
+    m_frame(nullptr),
+    m_decodeBufOffset(0),
+    m_config(name, iconFfmpeg, 86)
+{
+    m_packet.data = 0;
+}
 
 ffmpegBackend::~ffmpegBackend()
 {
-    close();
+    dl_av_frame_free(&m_frame);
+
+    if (m_packet.data)
+        dl_av_packet_unref(&m_packet);
+
+    dl_avcodec_free_context(&m_codecContext);
+    dl_avformat_close_input(&m_formatContext);
 }
 
 QString ffmpegBackend::getMetadata(const char* type)
@@ -267,8 +278,6 @@ QString ffmpegBackend::getMetadata(const char* type)
 
 bool ffmpegBackend::open(const QString& fileName)
 {
-    close();
-
     if (dl_avformat_open_input(&m_formatContext, fileName.toUtf8().constData(), nullptr, nullptr) != 0)
     {
         qWarning() << "Cannot open input";
@@ -285,8 +294,6 @@ bool ffmpegBackend::open(const QString& fileName)
         goto error;
 
     m_audioStream = m_formatContext->streams[m_audioStreamIndex];
-    m_decodeBufOffset = 0;
-    m_packet.data = 0;
     m_frame = dl_av_frame_alloc();
 
     switch(m_codecContext->sample_fmt)
@@ -337,24 +344,6 @@ error:
     dl_avformat_close_input(&m_formatContext);
 
     return false;
-}
-
-void ffmpegBackend::close()
-{
-    if (songLoaded().isEmpty())
-        return;
-
-    dl_av_frame_free(&m_frame);
-
-    if (m_packet.data)
-        dl_av_packet_unref(&m_packet);
-
-    dl_avcodec_free_context(&m_codecContext);
-    dl_avformat_close_input(&m_formatContext);
-
-    m_audioStream = nullptr;
-
-    songLoaded(QString());
 }
 
 bool ffmpegBackend::seek(int pos)

src/audio/input/ffmpegBackend.h
@@ -144,9 +144,6 @@ public:
     /// Open file
     bool open(const QString& fileName) override;
 
-    /// Close file
-    void close();
-
     /// Seek support
     bool seekable() const override { return true; }
 

src/audio/input/gmeBackend.h
@@ -137,9 +137,6 @@ public:
     /// Open file
     bool open(const QString& fileName) override;
 
-    /// Close file
-    void close();
-
     /// Rewind to start
     bool rewind() override;
 

src/audio/input/hvlBackend.cpp
@@ -165,13 +165,15 @@ hvlBackend::hvlBackend() :
 
 hvlBackend::~hvlBackend()
 {
-    close();
+    if (_tune)
+    {
+        hvl_FreeTune(_tune);
+        delete[] _buffer;
+    }
 }
 
 bool hvlBackend::open(const QString& fileName)
 {
-    close();
-
     _tune = hvl_LoadTune((TEXT*)fileName.toUtf8().constData(), m_config.samplerate(), 2);
 
     if (_tune == nullptr)
@@ -192,19 +194,6 @@ bool hvlBackend::open(const QString& fileName)
 
     songLoaded(fileName);
     return true;
-}
-
-void hvlBackend::close()
-{
-    if (_tune)
-    {
-        hvl_FreeTune(_tune);
-        _tune = nullptr;
-        delete[] _buffer;
-        _buffer = nullptr;
-    }
-
-    songLoaded(QString());
 }
 
 bool hvlBackend::rewind()

src/audio/input/hvlBackend.h
@@ -111,9 +111,6 @@ public:
     /// Open file
     bool open(const QString& fileName) override;
 
-    /// Close file
-    void close();
-
     /// Rewind to start
     bool rewind() override;
 

src/audio/input/mpcBackend.cpp
@@ -131,13 +131,16 @@ mpcBackend::mpcBackend() :
 
 mpcBackend::~mpcBackend()
 {
-    close();
+    _file.close();
+
+#ifdef MPCDEC_SV8
+    if (!songLoaded().isNull())
+        mpc_demux_exit(_demux);
+#endif
 }
 
 bool mpcBackend::open(const QString& fileName)
 {
-    close();
-
     _file.setFileName(fileName);
     if (!_file.open(QIODevice::ReadOnly))
         return false;
@@ -211,20 +214,7 @@ bool mpcBackend::open(const QString& fileName)
     return true;
 
 error:
-    close();
     return false;
-}
-
-void mpcBackend::close()
-{
-    _file.close();
-
-#ifdef MPCDEC_SV8
-    if (!songLoaded().isNull())
-        mpc_demux_exit(_demux);
-#endif
-
-    songLoaded(QString());
 }
 
 bool mpcBackend::seek(int pos)

src/audio/input/mpcBackend.h
@@ -114,9 +114,6 @@ public:
     /// Open file
     bool open(const QString& fileName) override;
 
-    /// Close file
-    void close();
-
     /// Seek support
     bool seekable() const override { return true; }
 

commit 69057d5d375c547d06edbdfd73a97cb65d58f469

gme: fix typo introduced with 94cdbcd

src/audio/input/gmeBackend.cpp
@@ -33,7 +33,7 @@
 
 #include <string>
 
-#define EXT "ay|gbs|gym|hes|f|nsfe|nsf|sap|spc|vgm|vgz"
+#define EXT "ay|gbs|gym|hes|kss|nsfe|nsf|sap|spc|vgm|vgz"
 
 // ASMA path to STIL.
 #define ASMA_STIL "/Docs/STIL.txt"