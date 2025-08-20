MusiQt/musiqt

commit 8bb94eb598da90ba946cf3ba1211e5513b4b2a66

Prevent possible memory leak

src/audio/player.cpp
@@ -99,19 +99,16 @@ bool player::tryPreload(const QString& song)
     if (songPreloaded.isEmpty())
         return false;
 
-    input* i = m_preload.release();
-    m_preload.reset(IFACTORY->get());
-
+    bool res = false;
     if (!songPreloaded.compare(song))
     {
-        m_input.reset(i);
+        m_input.reset(m_preload.get());
         emit songChanged();
-        return true;
+        res = true;
     }
-    else
-    {
-        return false;
-    }
+
+    m_preload.reset(IFACTORY->get());
+    return res;
 }
 
 void player::load(const QString& filename)

src/audio/player.h
@@ -25,8 +25,6 @@
 #include <QObject>
 #include <QScopedPointer>
 
-#include <memory>
-
 class audio;
 class input;
 
@@ -45,7 +43,7 @@ class player : public QObject
 private:
     QScopedPointer<input> m_input;
     QScopedPointer<audio> m_audio;
-    std::unique_ptr<input> m_preload;
+    QScopedPointer<input> m_preload;
 
 private:
     player(const player&) = delete;

commit 057c01db336ace3d28b7c37091d4a1e13e582d2b

Fix crash introduced with 8bb94eb

src/audio/player.cpp
@@ -30,10 +30,10 @@ player::player() :
     m_audio(new audio),
     m_preload(IFACTORY->get())
 {
-    connect(m_audio.data(), &audio::updateTime,  this, &player::updateTime);
-    connect(m_audio.data(), &audio::songEnded,   this, &player::songEnded);
-    connect(m_audio.data(), &audio::preloadSong, this, &player::preloadSong);
-    connect(m_audio.data(), &audio::audioError,  this, &player::onError);
+    connect(m_audio.get(), &audio::updateTime,  this, &player::updateTime);
+    connect(m_audio.get(), &audio::songEnded,   this, &player::songEnded);
+    connect(m_audio.get(), &audio::preloadSong, this, &player::preloadSong);
+    connect(m_audio.get(), &audio::audioError,  this, &player::onError);
 }
 
 player::~player() = default;
@@ -48,7 +48,7 @@ void player::play()
 {
     try
     {
-        if (m_audio->play(m_input.data()))
+        if (m_audio->play(m_input.get()))
             emit stateChanged();
     }
     catch (audio::initError const &e)
@@ -102,7 +102,7 @@ bool player::tryPreload(const QString& song)
     bool res = false;
     if (!songPreloaded.compare(song))
     {
-        m_input.reset(m_preload.get());
+        m_input.reset(m_preload.release());
         emit songChanged();
         res = true;
     }

src/audio/player.h
@@ -23,7 +23,8 @@
 #include "metaData.h"
 
 #include <QObject>
-#include <QScopedPointer>
+
+#include <memory>
 
 class audio;
 class input;
@@ -41,9 +42,9 @@ class player : public QObject
     Q_OBJECT
 
 private:
-    QScopedPointer<input> m_input;
-    QScopedPointer<audio> m_audio;
-    QScopedPointer<input> m_preload;
+    std::unique_ptr<input> m_input;
+    std::unique_ptr<audio> m_audio;
+    std::unique_ptr<input> m_preload;
 
 private:
     player(const player&) = delete;